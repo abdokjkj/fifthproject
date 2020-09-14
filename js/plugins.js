f9ad3736b18dd2772b690da06b19b5174e7b29e3
abdokjkj <71026403+abdokjkj@users.noreply.github.com>
2020-09-14 13:47:16 +0200

    Add files via upload

---
+++ js/plugins.js
@@ -0,0 +1,22 @@
+
+window.onscroll = function(){
+
+"use strict";
+
+$(".header .ul ul").css({
+
+width : '750' ,
+height : '50' ,
+backgroundColor : 'black' ,
+textAlign : 'center' 
+        
+})
+
+}
+
+
+    
+
+
+
+
